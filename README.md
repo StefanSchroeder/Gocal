9bc95af598531a9893d3dc055f161a83c13e50c5
Stefan <Stefan>
2014-03-26 13:14:24 -0400

    More features.

--- README.md
+++ README.md
@@ -1,37 +1,161 @@
-Warning: Gocal is pre-alpha and released here in good faith. Don't use it to control nuclear power plants.
 
 Gocal
 =====
 
-Gocal is a simple clone of pcal. It's a tool to create monthly calendars in PDF with a no-nonsense attitude.
-Right now it creates a 12-page PDF with one month per page. Pcal has quite a few options, that gocal will
-never support, like calculating Easter. 
+Gocal is a simple clone of pcal. It's a standalone tool to create monthly calendars in PDF with a no-nonsense attitude.
+It creates by default a 12-page PDF with one month per page for the current year. 
+Alternatively the following arguments are supported:
 
-Features:
+  gocal YEAR => Create a 12-page calendar for YEAR
+
+  gocal MONTH YEAR => Create a 1-page calendar for the MONTH in YEAR
+
+  gocal BEGIN-MONTH END-MONTH YEAR => Create a sequence from BEGIN-MONTH to END-MONTH in YEAR
+
+Gocal has a few gimmicks:
 
 * PDF generation
-* Week number on every Monday
+* Optional week number on every Monday
+* Optional day of year number on every day
+* Optional moon phases
 * Events in XML configuration file
-* No other languages than English currently
-* No other 'first day of the week' than Monday
+* Several languages 
+* Wallpaper option
+* Photo calendar option (from single image or directory)
+* Page orientation and paper size option
+* Font selection (limited to Times, Helvetica/Arial, Courier)
 
-Why not using PCal? There are several reasons. One, I couldn't build it on Windows, next, I don't care about Postscript any longer, third, a calendar seems to be exactly a kind of project that I am able to handle from a complexity perspective as a single developer.
+Pcal has quite a few options, that gocal will never support, like calculating Easter. 
 
+Why not using pcal or gcal? There are several reasons. One, I couldn't build it on 
+Windows, next, I don't care about Postscript any longer, third, a calendar seems to 
+be exactly a kind of project that I am able to handle from a complexity perspective 
+as a single developer.
 
-The main idea of gocal is that you print one or more month and use it to add 
-handwritten notes to it.
+The main idea of gocal is simplicity. While it is absolutely possible to create
+an application where every single stroke is configurable, I believe that most of
+you are too busy to play around with lots of options and want a pleasant calendar
+out-of-the-box. That's what gocal provides.
 
 ![Logo](http://github.com/StefanSchroeder/Gocal/blob/master/screenshot.png?raw=true)
+
+The power of gocal is based on the cool libraries that it includes. This implies that
+several of the options are actually options of the libraries, e.g. the paper format (gofpdf) or
+the supported languages (goodsign/monday).
+
+It is suggested to hide some of the optional fields or the cells will look crowded.
+
+
+Build instructions
+==================
+
+Run 
+
+  go get github.com/StefanSchroeder/gocal
+
+Gocal has a quite a few dependencies that go should resolve automatically.
+
+Build with
+
+  go build
+
+
+Options
+=======
+
+  -f="Gocal": Footer note
+
+Change the string at the bottom of the page.
+
+  -font="Cabalett": Main font (Times/Arial/Courier)
+
+Set the font. The fonts times, arial/helvetica and courier are built-in in PDF.
+Other fonts are currently not supported, but these fonts should be sufficient for
+all practical purposes. As a proof of concept I included the freeware Cabalett-font by
+Harold Lohner (http://haroldsfonts.com/) which is really beautiful.
+
+  -lang="": Language
+
+Gocal reads the LANG environment variable. If it matches one of 
+
+		en_US en_GB nl_BE nl_NL fi_FI fr_FR fr_CA de_DE
+
+the library goodsign/monday is used to translate the weekday names and month names.
+Although this library supports a few other languages, I found that many of the 
+languages do not work. The language from the environment can be overridden with this
+parameter. If your LANG is not recognized, we default to en_US.
+
+  -nodoy=false: Hide day of year (false)
+
+  -noevents=false: Hide events from config file (false)
+
+Gocal adds events from an XML file in the current directory to dates. This option
+disables this feature.
+
+  -nomoon=false: Hide moon phases (false)
+  -noweek=false: Hide week number (false)
+
+The week number according to ISO-8601, added on every Monday.
+
+  -o="output.pdf": Output filename
+  -p="L": Orientation (L)andscape/(P)ortrait
+
+Typically you want landscape for calendars without image and portrait for calendars with image.
+
+  -paper="A4": Paper format (A3 A4 A5 Letter Legal)
+  -photo="": Show photo (single image PNG JPG GIF)
+
+This option will add this image to every month.
+
+  -photos="": Show photos (directory PNG JPG GIF)
+
+This option will add the twelve first files as images to the twelve month.
+If less than twelve files are found, the sequence will re-start after the last image.
+
+  -wall="": Show wallpaper PNG JPG GIF
+
+This option will add this image to every month as a background. You should only 
+use images with a bright tone so that you do not obstruct the usefulness of the calendar.
+
+
+Examples
+========
+
+ 
+![Logo](http://github.com/StefanSchroeder/Gocal/blob/master/screen0.png?raw=true)
+
+German locale with font Cabaletta and gopher wallpaper.
+
+![Logo](http://github.com/StefanSchroeder/Gocal/blob/master/screen1.png?raw=true)
+
+Finnish locale with all gimmicks hidden.
+
+![Logo](http://github.com/StefanSchroeder/Gocal/blob/master/screen2.png?raw=true)
+
+English locale with default settings and Cabaletta font.
+
+![Logo](http://github.com/StefanSchroeder/Gocal/blob/master/screen3.png?raw=true)
+
+English locale in photo-mode.
+
+![Logo](http://github.com/StefanSchroeder/Gocal/blob/master/screen4.png?raw=true)
+
+Finnish locale with times font (landscape).
+
+![Logo](http://github.com/StefanSchroeder/Gocal/blob/master/screen5.png?raw=true)
+
+English locale with times font (portrait). 
+
 
 Roadmap
 =======
 
 It would be really cool to allow gocal to be a drop-in replacement for pcal, OTOH why bother?
 
-Also, I think that gocal would be a good sample project to study i18n with Go.
-
 
 Known bugs:
 ===========
 
-* I dropped sunrise/sunset.
+* When you have multiple events on the same date, they are overlapping.
+* Not all text will fit into the cells with some settings.
+
--- gocal.go
+++ gocal.go
@@ -2,27 +2,35 @@
 
 This is gocal a tool to generate calendars in PDF for printing.
 
-* inspired by pcal
-* simplicity: create a nice calendar programmatically with minimum effort.
-* show week number
-* moonphase
-* add events from configuration file
-* set papersize
+* Inspired by pcal
+* Simplicity: create a nice calendar programmatically with minimum effort.
+* One argument: Year
+* Two argument: Month Year
+* Two argument: MonthBegin MonthEnd Year
+* Week number
+* Moonphase
+* Add events from configuration file
+* Set papersize
+* Choose fonts (limited)
+* Several languages
+* day of year/remaining
+* bg image
+* Photo calendar
+*
 * add month notes
-* choose fonts
-* add images to days
-* multilang
-* sunrise/sunset for location
-
 */
 
 package main
 
 import (
+	"bytes"
+	"code.google.com/p/go-charset/charset"
+	_ "code.google.com/p/go-charset/data"
 	"code.google.com/p/gofpdf"
 	"encoding/xml"
 	"flag"
 	"fmt"
+	"github.com/goodsign/monday"
 	"github.com/soniakeys/meeus/base"
 	"github.com/soniakeys/meeus/julian"
 	"github.com/soniakeys/meeus/moonillum"
@@ -30,6 +38,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
@@ -44,21 +53,40 @@ const (
 	darkgrey      = 150
 	lightgrey     = 170
 	black         = 0
-	moonSize      = 4.0
 	MARGIN        = 10.0
-	MAINFONT      = "Times"
 	eventFontsize = 10
+	loc           = "de_DE"
 )
 
-var orientation = flag.String("p", "L", "Orientation (L)andscape/(P)ortrait")
+var optOrientation = flag.String("p", "L", "Orientation (L)andscape/(P)ortrait")
 var outfilename = flag.String("o", "output.pdf", "Output filename")
-var footer = flag.String("f", "Gocal", "Footer note")
+var optFooter = flag.String("f", "Gocal", "Footer note")
+var optLocale = flag.String("lang", "", "Language")
+var optFont = flag.String("font", "Cabalett", "Main font (Times/Arial/Courier)")
+var optHideWeek = flag.Bool("noweek", false, "Hide week number (false)")
+var optHideEvents = flag.Bool("noevents", false, "Hide events from config file (false)")
+var optHideMoon = flag.Bool("nomoon", false, "Hide moon phases (false)")
+var optHideDOY = flag.Bool("nodoy", false, "Hide day of year (false)")
+var optWallpaper = flag.String("wall", "", "Show wallpaper PNG JPG GIF")
+var optPhoto = flag.String("photo", "", "Show photo (single image PNG JPG GIF)")
+var optPhotos = flag.String("photos", "", "Show photos (directory PNG JPG GIF)")
+var optPaper = flag.String("paper", "A4", "Paper format (A3 A4 A5 Letter Legal)")
+var moonSize = 4.0
+var photoList [13]string
+
+// This map stores all the languages we know
+// and true for those we support.
 
 type gocalDate struct {
 	Month   time.Month
 	Day     int
 	Text    string
 	Weekday string
+}
+
+type monthRange struct {
+	begin int
+	end   int
 }
 
 type pdfWriter struct {
@@ -79,7 +107,7 @@ func (pw *pdfWriter) Close() (err error) {
 		pw.fl = nil
 	}
 	if pw.pdf.Ok() {
-		fmt.Printf("Successfully generated \n")
+		fmt.Printf("Generated '%v'.\n", *outfilename)
 	} else {
 		fmt.Printf("%s\n", pw.pdf.Error())
 	}
@@ -93,7 +121,7 @@ func docWriter(pdf *gofpdf.Fpdf) *pdfWriter {
 		var err error
 		pw.fl, err = os.Create(*outfilename)
 		if err != nil {
-			pdf.SetErrorf("Error opening output file ")
+			pdf.SetErrorf("Error opening output file.")
 		}
 	}
 	return pw
@@ -165,15 +193,127 @@ func readConfigurationfile(filename string) (eL []gocalDate) {
 	return eL
 }
 
+/// This function returns an array of Monthnames already in the
+// right locale.
+func getLocalizedMonthNames(locale string) (monthnames [13]string) {
+
+	for page := 1; page < 13; page++ {
+		t := time.Date(2013, time.Month(page), 1, 0, 0, 0, 0, time.UTC)
+		buf := new(bytes.Buffer)
+		w, err := charset.NewWriter("windows-1252", buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		monthString := fmt.Sprintf("%s", monday.Format(t, "January", monday.Locale(locale)))
+		fmt.Fprintf(w, monthString)
+		w.Close()
+
+		monthnames[page] = fmt.Sprintf("%s", buf)
+	}
+
+	return monthnames
+}
+
+func getLocalizedWeekdayNames(locale string) (wdnames [8]string) {
+	for i := 1; i <= 7; i++ {
+		// Some arbitrary date, that allows us to pickup Weekday-Strings.
+		t := time.Date(2013, 1, 6+i, 0, 0, 0, 0, time.UTC)
+		wdnames[i] = monday.Format(t, "Monday", monday.Locale(locale))
+	}
+	return wdnames
+}
+
+func computeMoonphases(moon map[int]string, da int, mo int, yr int) {
+	daysInYear := 365
+	if julian.LeapYearGregorian(yr) {
+		daysInYear = 366
+	}
+	// This is a perfect example for the difference btw. efficient and
+	// effective. It's effective, but not efficient:
+	// Look at every day and check if it has any of the Moon Phases.
+	for i := 0; i < 32; i++ {
+		dayOfYear := julian.DayOfYearGregorian(yr, mo, int(da)+i)
+		decimalYear := float64(yr) +
+			float64(dayOfYear-1)/float64(daysInYear)
+		jdeNew := moonphase.New(decimalYear)
+		y, m, d := julian.JDToCalendar(jdeNew)
+		if (y == yr) && (m == mo) && (int(d) == i) {
+			//fmt.Printf("New moon on %d\n", int(d))
+			moon[int(d)] = "New"
+		}
+		jdeNew = moonphase.Full(decimalYear)
+		y, m, d = julian.JDToCalendar(jdeNew)
+		if (y == yr) && (m == mo) && (int(d) == i) {
+			//fmt.Printf("Full moon on %d\n", int(d))
+			moon[int(d)] = "Full"
+		}
+		jdeNew = moonphase.First(decimalYear)
+		y, m, d = julian.JDToCalendar(jdeNew)
+		if (y == yr) && (m == mo) && (int(d) == i) {
+			//fmt.Printf("First Q moon on %d\n", int(d))
+			moon[int(d)] = "First"
+		}
+		jdeNew = moonphase.Last(decimalYear)
+		y, m, d = julian.JDToCalendar(jdeNew)
+		if (y == yr) && (m == mo) && (int(d) == i) {
+			moon[int(d)] = "Last"
+			//fmt.Printf("Last Q moon on %d\n", int(d))
+		}
+	}
+}
+
 func main() {
 	flag.Parse()
 
+	testedLanguage := map[string]bool{
+		"en_US": true,
+		"en_GB": true,
+		"da_DK": false,
+		"nl_BE": true,
+		"nl_NL": true,
+		"fi_FI": true,
+		"fr_FR": true,
+		"fr_CA": true,
+		"de_DE": true,
+		"hu_HU": false,
+		"it_IT": false,
+		"nn_NO": false,
+		"nb_NO": false,
+		"pt_PT": false,
+		"pt_BR": false,
+		"ro_RO": false,
+		"ru_RU": false,
+		"es_ES": false,
+		"sv_SE": false,
+		"tr_TR": false,
+		"bg_BG": false,
+		"zh_CN": false,
+		"zh_TW": false,
+		"zh_HK": false,
+	}
+
+	// First try Environment
+	currentLanguage := os.Getenv("LANG")
+
+	// If set on the cmdline, override
+	if *optLocale != "" {
+		currentLanguage = *optLocale
+	}
+
+	// if we don't know that language, fall back to en.
+	if testedLanguage[currentLanguage] != true {
+		currentLanguage = "en_US"
+	}
+
 	eventList := make([]gocalDate, 1000)
 
-	eventList = readConfigurationfile(CONFIGFILE)
+	if *optHideEvents == false {
+		eventList = readConfigurationfile(CONFIGFILE)
+	}
 
 	var wantyear int = int(time.Now().Year())
-	var wantmonth int = int(time.Now().Month())
+	wantmonths := monthRange{1, 12}
 
 	if flag.NArg() == 1 {
 		dummyyear, _ := strconv.ParseInt(flag.Arg(0), 10, 32)
@@ -181,84 +321,84 @@ func main() {
 	} else if flag.NArg() == 2 {
 		dummymonth, _ := strconv.ParseInt(flag.Arg(0), 10, 32)
 		dummyyear, _ := strconv.ParseInt(flag.Arg(1), 10, 32)
-		wantmonth = int(dummymonth)
+		wantmonths.begin = int(dummymonth)
+		wantmonths.end = int(dummymonth)
+		wantyear = int(dummyyear)
+	} else if flag.NArg() == 3 {
+		dummymonthBegin, _ := strconv.ParseInt(flag.Arg(0), 10, 32)
+		dummymonthEnd, _ := strconv.ParseInt(flag.Arg(1), 10, 32)
+		dummyyear, _ := strconv.ParseInt(flag.Arg(2), 10, 32)
+		wantmonths.begin = int(dummymonthBegin)
+		wantmonths.end = int(dummymonthEnd)
 		wantyear = int(dummyyear)
 	}
 
-	paperformat := "A4"
+	localizedMonthNames := getLocalizedMonthNames(currentLanguage)
+	localizedWeekdayNames := getLocalizedWeekdayNames(currentLanguage)
 
-	pdf := gofpdf.New(*orientation, "mm", paperformat, cnFontDir)
+	paperformat := *optPaper
+
+	pdf := gofpdf.New(*optOrientation, "mm", paperformat, cnFontDir)
+	pdf.SetTitle("Created with Gocal", true)
+	pdf.AddFont("CABALETT", "", "CABALETT.json")
+
 	PAGEWIDTH, PAGEHEIGHT, _ := pdf.PageSize(0)
-	if *orientation != "P" {
+	if *optOrientation != "P" {
 		PAGEWIDTH, PAGEHEIGHT = PAGEHEIGHT, PAGEWIDTH
 	}
 
+	// FIXME : Geometry is here!
 	cw := (PAGEWIDTH - 2*MARGIN) / COLUMNS // cellwidth w 20mm margin
 	ch := PAGEHEIGHT / (LINES + 2)         // cellheight
-	calendarTable := func(mymonth int) {
-		wantmonth = mymonth
-		for titleDay := 1; titleDay < 8; titleDay++ { // Print Weekdays
-			pdf.CellFormat(cw, 7, fmt.Sprintf("%s", time.Weekday(titleDay%7)), "0", 0, "C", false, 0, "")
+
+	YOFFSET := 0.0
+	if *optPhoto != "" {
+		ch *= 0.5
+		moonSize *= 0.6 // make moon smaller on photopage
+		for i := 0; i < 13; i++ {
+			photoList[i] = *optPhoto
+		}
+	}
+	if *optPhotos != "" {
+		ch *= 0.5
+		moonSize *= 0.6 // make moon smaller on photopage
+		a, _ := filepath.Glob(*optPhotos + "/*")
+		for i := 0; i < 13; i++ {
+			photoList[i] = a[i%len(a)]
+		}
+	}
+
+	calendarTable := func(mymonth int, myyear int) {
+		for weekday := 1; weekday < 8; weekday++ { // Print Weekdays
+			pdf.CellFormat(cw, 7, localizedWeekdayNames[weekday], "0", 0, "C", false, 0, "")
 		}
 		pdf.Ln(-1)
-		var day int64 = 1
 
-		t := time.Date(wantyear, time.Month(wantmonth), 1, 0, 0, 0, 0, time.UTC)
+		// Figure out the first day in the calendar which depends on the weekday
+		// of the first day
+		var day int64 = 1
+		t := time.Date(myyear, time.Month(mymonth), 1, 0, 0, 0, 0, time.UTC)
+
 		day -= int64(t.Weekday())
 		if day > 0 { // adjust silly exception where month starts w/ Sunday.
 			day -= 7
 		}
 
-		daysInYear := 365
-		if julian.LeapYearGregorian(wantyear) {
-			daysInYear = 366
-		}
-
 		moon := make(map[int]string)
-
-		// This is a perfect example for the difference btw. efficient and
-		// effective. It's effective, but not efficient:
-		// Look at every day and check if it has any of the Moon Phases.
-		for i := 0; i < 32; i++ {
-			decimalYear := float64(wantyear) +
-				float64(julian.DayOfYearGregorian(wantyear, wantmonth, int(day)+i)-1)/float64(daysInYear)
-			jdeNew := moonphase.New(decimalYear)
-			y, m, d := julian.JDToCalendar(jdeNew)
-			if (y == wantyear) && (m == wantmonth) && (int(d) == i) {
-				//fmt.Printf("New moon on %d\n", int(d))
-				moon[int(d)] = "New"
-			}
-			jdeNew = moonphase.Full(decimalYear)
-			y, m, d = julian.JDToCalendar(jdeNew)
-			if (y == wantyear) && (m == wantmonth) && (int(d) == i) {
-				//fmt.Printf("Full moon on %d\n", int(d))
-				moon[int(d)] = "Full"
-			}
-			jdeNew = moonphase.First(decimalYear)
-			y, m, d = julian.JDToCalendar(jdeNew)
-			if (y == wantyear) && (m == wantmonth) && (int(d) == i) {
-				//fmt.Printf("First Q moon on %d\n", int(d))
-				moon[int(d)] = "First"
-			}
-			jdeNew = moonphase.Last(decimalYear)
-			y, m, d = julian.JDToCalendar(jdeNew)
-			if (y == wantyear) && (m == wantmonth) && (int(d) == i) {
-				moon[int(d)] = "Last"
-				//fmt.Printf("Last Q moon on %d\n", int(d))
-			}
+		if *optHideMoon == false {
+			computeMoonphases(moon, int(day), mymonth, myyear)
 		}
-
-		pdf.SetFont(MAINFONT, "B", 24)
+		pdf.SetFont(*optFont, "", 24)
 		for i := 0; i < LINES; i++ {
 			for j := 0; j < COLUMNS; j++ {
 				var fill bool = false
-				nd := time.Date(wantyear, time.Month(wantmonth), 1, 0, 0, 0, 0, time.UTC).Add(time.Duration(day) * 24 * 60 * 60 * time.Second)
+				nd := time.Date(myyear, time.Month(mymonth), 1, 0, 0, 0, 0, time.UTC).Add(time.Duration(day) * 24 * 60 * 60 * time.Second)
 
 				// Determine color
-				if nd.Month() != time.Month(wantmonth) { // GREY
+				if nd.Month() != time.Month(mymonth) { // GREY
 					pdf.SetTextColor(darkgrey, darkgrey, darkgrey)
 					pdf.SetFillColor(lightgrey, lightgrey, lightgrey)
-					fill = true
+					fill = false // FIXME
 				} else if nd.Weekday() == time.Saturday || nd.Weekday() == time.Sunday {
 					pdf.SetTextColor(255, 0, 0) // RED
 				} else {
@@ -267,12 +407,8 @@ func main() {
 
 				pdf.SetCellMargin(1)
 
-				// Add day-of-month number
-				cellString := fmt.Sprintf("%d", nd.Day())
-
 				// Add moon icon
 				if m, ok := moon[int(day)]; ok == true {
-					// Moonphase as Text: cellString += " " + m
 					x, y := pdf.GetXY()
 					moonLocX, moonLocY := x+cw*0.82, y+ch*0.2
 
@@ -288,30 +424,39 @@ func main() {
 					}
 				}
 
-				// Add week number
-				if nd.Weekday() == time.Monday {
+				// Day of year, lower right
+				if *optHideDOY == false && int(nd.Month()) == mymonth {
+					doy := julian.DayOfYearGregorian(myyear, mymonth, int(nd.Day()))
 					x, y := pdf.GetXY()
-					pdf.SetFont(MAINFONT, "", 12)
+					pdf.SetFont(*optFont, "", 12)
+					pdf.Text(x+0.82*cw, y+0.95*ch, fmt.Sprintf("%d", doy))
+				}
+
+				// Add week number, lower left
+				if nd.Weekday() == time.Monday && *optHideWeek == false {
+					x, y := pdf.GetXY()
+					pdf.SetFont(*optFont, "", 12)
 					_, weeknr := nd.ISOWeek()
-					pdf.Text(x+0.02*cw, y+0.95*ch, fmt.Sprintf("Week %d", weeknr))
+					pdf.Text(x+0.02*cw, y+0.95*ch, fmt.Sprintf("W %d", weeknr))
 				}
 
 				// Add event text
 				for _, ev := range eventList {
 					if nd.Day() == ev.Day && nd.Month() == ev.Month {
 						x, y := pdf.GetXY()
-						pdf.SetFont(MAINFONT, "", eventFontsize)
+						pdf.SetFont(*optFont, "", eventFontsize)
 
 						textArray := strings.Split(ev.Text, "\n")
 
 						for i, j := range textArray {
-							pdf.Text(x+0.02*cw, y+0.50*ch+float64(i)*eventFontsize/4.0, fmt.Sprintf("%s", j))
+							pdf.Text(x+0.02*cw, y+0.70*ch+float64(i)*eventFontsize/4.0, fmt.Sprintf("%s", j))
 						}
 					}
 				}
 
-				pdf.SetFont(MAINFONT, "B", 32)
-				pdf.CellFormat(cw, ch, cellString, "1", 0, "TL", fill, 0, "")
+				// day of the month, big number
+				pdf.SetFont(*optFont, "", 32)
+				pdf.CellFormat(cw, ch, fmt.Sprintf("%d", nd.Day()), "1", 0, "TL", fill, 0, "")
 				day++
 			}
 			pdf.Ln(-1)
@@ -319,20 +464,25 @@ func main() {
 
 	}
 
-	for page := 1; page < 13; page++ {
+	for mo := wantmonths.begin; mo <= wantmonths.end; mo++ {
 		//fmt.Printf("Printing page %d\n", page)
-		wantmonth = page
 		pdf.AddPage()
-		pdf.SetTextColor(black, black, black) // BLACK
-		pdf.SetFont(MAINFONT, "B", 32)
-		pdf.CellFormat(PAGEWIDTH-10.0, 10.0, fmt.Sprintf("%s", time.Month(wantmonth))+" "+fmt.Sprintf("%d", wantyear), "", 0, "C", false, 0, "")
+		if *optWallpaper != "" {
+			pdf.Image(*optWallpaper, 0, 0, PAGEWIDTH, PAGEHEIGHT, false, "", 0, "")
+		}
+		if *optPhoto != "" || *optPhotos != "" {
+			pdf.Image(photoList[mo-1], 0, PAGEHEIGHT*0.5, cw*8, ch*8, false, "", 0, "")
+		}
+		pdf.SetTextColor(black, black, black)
+		pdf.SetFont(*optFont, "", 32)
+
+		pdf.CellFormat(PAGEWIDTH-10.0, YOFFSET+10, localizedMonthNames[mo]+" "+fmt.Sprintf("%d", wantyear), "", 0, "C", false, 0, "")
 		pdf.Ln(-1)
-		pdf.SetTextColor(black, black, black) // BLACK
-		pdf.SetFont(MAINFONT, "B", 16)
-		calendarTable(page)
+		pdf.SetFont(*optFont, "", 16)
+		calendarTable(mo, wantyear)
 		pdf.Ln(-1)
-		pdf.SetFont(MAINFONT, "B", 12)
-		pdf.Text(0.50*PAGEWIDTH, 0.95*PAGEHEIGHT, fmt.Sprintf("%s", *footer))
+		pdf.SetFont(*optFont, "", 12)
+		pdf.Text(0.50*PAGEWIDTH, 0.95*PAGEHEIGHT, fmt.Sprintf("%s", *optFooter))
 	}
 
 	pdf.OutputAndClose(docWriter(pdf))
