e39086de085e0fc09028c2d2c80a51c68a207cd6
Stefan Schroeder <ondekoza@gmail.com>
2016-03-24 20:44:08 +0100

    Fixed gone code.google.com repos.

--- util.go
+++ util.go
@@ -14,11 +14,10 @@ package gocal
 
 import (
 	"bytes"
-	"code.google.com/p/go-charset/charset"
+	"github.com/rogpeppe/go-charset/charset"
 	"encoding/xml"
 	"fmt"
-	//	"code.google.com/p/gofpdf"
-	_ "code.google.com/p/go-charset/data"
+	_ "github.com/rogpeppe/go-charset/charset/data"
 	"github.com/goodsign/monday"
 	"github.com/jung-kurt/gofpdf"
 	"github.com/soniakeys/meeus/julian"
